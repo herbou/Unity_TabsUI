47c68d462e7ec1db3379179e500a7a46c67d7de4
hamza herbou <hamza95herbou@gmail.com>
2022-02-03 12:42:44 +0100

    Update TabsUIHorizontal.cs

--- Assets/UITabs/scripts/TabsUIHorizontal.cs
+++ Assets/UITabs/scripts/TabsUIHorizontal.cs
@@ -5,9 +5,6 @@ using EasyUI.Tabs;
 public class TabsUIHorizontal : TabsUI
 {
     #if UNITY_EDITOR
-    private void Reset() {
-        OnValidate();
-    }
     private void OnValidate() {
         base.Validate(TabsType.Horizontal);
     }
